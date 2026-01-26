26ad3046edf935dc7f852366b66c6a448900a557
achyut777 <achyuthadwani32@gmail.com>
2026-01-26 13:03:01 +0530

    Add files via upload

---
+++ vm.ps1
@@ -0,0 +1,26 @@
+# Variables
+$resourceGroup = "RG-Achyut"
+$location      = "EastUS"
+$vmName        = "AchyutVM"
+$vmSize        = "Standard_B1s"
+$adminUser     = "azureuser"
+
+# Create Resource Group
+New-AzResourceGroup `
+    -Name $resourceGroup `
+    -Location $location
+
+# Create VM (Azure will auto-create VNet, Subnet, NSG, Public IP)
+New-AzVM `
+    -ResourceGroupName $resourceGroup `
+    -Name $vmName `
+    -Location $location `
+    -VirtualNetworkName "AchyutVNet" `
+    -SubnetName "AchyutSubnet" `
+    -SecurityGroupName "AchyutNSG" `
+    -PublicIpAddressName "AchyutPublicIP" `
+    -OpenPorts 22 `
+    -Image "Ubuntu2204" `
+    -Size $vmSize `
+    -Credential (Get-Credential) `
+    -Verbose
